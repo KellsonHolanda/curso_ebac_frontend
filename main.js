66a1689aaf2406a4d35d2bde998fcc5e7cb0f91f
kellsonHolanda <kellson7777@gmail.com>
2023-12-05 17:45:40 -0300

    tratando exceções

--- main.js
+++ main.js
@@ -1,32 +1,50 @@
-// document.addEventListener("DOMContentLoaded", function(){
-//     document.getElementById('btn-buscar-cep').addEventListener('click', function(){
-//         const xhttp = new XMLHttpRequest();
-//         const cep = document.getElementById('cep').ariaValueMax;
-//         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
-//         xhttp.open('GET', endpoint);
-//         xhttp.send();
-//     })
-// })
 
-$(document).ready(function(){
-    $('#btn-buscar-cep').click(function(){
+$(document).ready(function () {
+    $('#cep').mask('00000-000');
+    $('#btn-buscar-cep').click(function () {
         const cep = $('#cep').val();
         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
         const botao = $(this);
         $(botao).find('i').addClass('d-none');
         $(botao).find('span').removeClass('d-none');
+        console.log(endpoint);
 
+        fetch(endpoint)
+            .then(function (resposta) {
+                return resposta.json();
+            })
+            .then(function (json) {
+                const logradouro = json.logradouro;
+                const bairro = json.bairro;
+                const cidade = json.localidade;
+                const estado = json.uf;
+                const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+                $('#endereco').val(endereco);
+                setTimeout(function () {
+                    $(botao).find('i').removeClass('d-none');
+                    $(botao).find('span').addClass('d-none');
 
+                }, 1000);
+            })
+            .catch(function(erro){
+                alert("Por favor, digite um CEP válido.")
+            })
+            .finally(function(){
+                $(botao).find('i').removeClass('d-none');
+                $(botao).find('span').addClass('d-none');
 
-        $.ajax(endpoint).done(function(resposta){
-            const logradouro = resposta.logradouro;
-            const bairro = resposta.bairro;
-            const cidade = resposta.cidade;
-            const estado = resposta.estado;
-            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
-            $('#endereco').val(endereco);
-            $(botao).find('i').removeClass('d-none');
-            $(botao).find('span').addClass('d-none');
-        })
+            })
     })
+    $('#formulario').submit(function(e){
+        e.preventDefault();
+        if ($('#nome').val().length == 0) {
+                throw new Error("Nome");
+                
+            
+            
+        }
+    })
+
+
+    
 })
