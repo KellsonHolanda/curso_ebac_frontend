4f7109a0632de83c03b54929324c0e487648c078
kellsonHolanda <117248477+kellsonHolanda@users.noreply.github.com>
2024-01-18 10:15:14 -0300

    inclusão inicial de arquivos

---
+++ exercicio_ts/src/tarefa.js
@@ -0,0 +1,9 @@
+"use strict";
+const multiplicacao = function (num1, num2) {
+    return num1 * num2;
+};
+console.log(multiplicacao(2, 13));
+const saudacao = function (nome) {
+    return `Olá ${nome}`;
+};
+console.log(saudacao("Fulano"));
---
+++ exercicio_ts/src/tarefa.ts
@@ -0,0 +1,9 @@
+const multiplicacao = function (num1:number, num2: number): number {
+    return num1*num2;
+} 
+console.log(multiplicacao(2,13));
+
+const saudacao = function (nome: string): string{
+    return `Olá ${nome}`;
+}
+console.log(saudacao("Fulano"));
